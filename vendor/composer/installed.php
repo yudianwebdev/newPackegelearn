ce52de5aaea99ae3683b70b66acbd3f1d2c6167f
yudian nanda <yudiannanwebdev@gmail.com>
2022-04-12 13:24:38 +0700

    fix: ini membuat istilahnya componen buar respones api

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '23a3f0b092150401061837f7576a4cf88ed9b6cf',
+        'name' => 'malikwebdev/newpackeg',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'malikwebdev/newpackeg' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '23a3f0b092150401061837f7576a4cf88ed9b6cf',
+            'dev_requirement' => false,
+        ),
+    ),
+);
